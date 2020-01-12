5e5ef58727622ac74fbbae5cf90d3aff5844f11d
Gordon Yeh <gordon.yeh.dev@gmail.com>
2020-01-11 22:11:19 -0800

    add base for frontend

---
+++ client/src/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Queue Up!</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </nav>
+  );
+};
+
+export default Navbar;
