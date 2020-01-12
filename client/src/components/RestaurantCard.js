a01a8ca2a34351de72062d08a9373872cae65aa3
Gordon Yeh <gordon.yeh.dev@gmail.com>
2020-01-12 01:58:27 -0800

    homepage done, does not look good on mobile

--- client/src/components/RestaurantCard.js
+++ client/src/components/RestaurantCard.js
@@ -5,14 +5,14 @@ export default function RestaurantCard(props) {
   return (
     <div className="restaurant-card">
       <div className="card">
-        <div className="card-img" >
+        <div className="card-img" style={{'background': `url(${props.coverUrl})`}}>
           <div className="card-title">
             <h3>{props.name}</h3>
             {`${props.distance}km from you`}
           </div>
           <div className="card-tags">
-            <span class="badge badge-secondary">{`${props.time[0]}~${props.time[1]}`} min<div>wait time</div></span>
-            <span class="badge badge-secondary">{props.ahead}<div>people ahead</div></span>
+            <span class="badge badge-dark">{`${props.time[0]}~${props.time[1]}`} min<div>wait time</div></span>
+            <span class="badge badge-dark">{props.ahead}<div>people ahead</div></span>
           </div>
         </div>
         <div className="card-body">
