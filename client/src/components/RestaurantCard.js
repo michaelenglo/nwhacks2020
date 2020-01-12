8c415583428f5e96376fc360a863c913643c09c6
Gordon Yeh <gordon.yeh.dev@gmail.com>
2020-01-11 23:59:09 -0800

    add way to server img

--- client/src/components/RestaurantCard.js
+++ client/src/components/RestaurantCard.js
@@ -5,6 +5,7 @@ export default function RestaurantCard(props) {
   return (
     <div class="restaurant-card">
       <div class="card">
+        {'heelo' + process.env.API_ENDPOINT}
         <img src="./test-logo.png" class="card-img-top" alt="..."/>
         <div class="card-body">
           <button type="button" class="btn btn-link btn-lg btn-block"><h5 class="card-title">Queue Up!</h5></button>
