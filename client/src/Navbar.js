173a27d13cd8bcc6cd81e41eb000ef13a8fb775c
Gordon Yeh <gordon.yeh.dev@gmail.com>
2020-01-12 08:21:54 -0800

    change logo

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import 'Navbar.scss';
+import env from 'env';
 
 export default function Navbar() {
   return (
     <nav class="navbar navbar-expand-lg">
-      <a class="navbar-brand" href="/">Queue Up!</a>
+      <a class="navbar-brand" href="/"><img width="125"src={`${env.IMG_ENDPOINT}/queueUp.svg`} alt="Queue Up!"/></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
